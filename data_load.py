754559a31505f40d73f0132389d9248586f10e7c
nimitm97 <48625136+nimitm97@users.noreply.github.com>
2021-02-17 20:48:38 +0100

    Loading and transforming Dataset
    Performing transforms on input image and loading it in the Dataset before starting training

---
+++ data_load.py
@@ -0,0 +1,103 @@
+import glob
+import os
+import torch
+from torch.utils.data import dataset, dataloader
+import matplotlib.image as mpimg
+import numpy as np
+import cv2
+
+
+class DogsCatsDataset:
+    def __init__(self, root_dir, transform=None):
+        """
+        :param root_dir: directory to images
+        :param transform: Optional transform to be applied to images
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(os.listdir(self.root_dir))
+
+    def __getitem__(self, idx):
+        images_list = os.listdir(self.root_dir)
+        image_name = images_list[idx]
+        image = mpimg.imread(image_name)
+
+        if image.shape[2] == 4:
+            image = image[:, :, 0:3]
+
+        category_list = image_name.split(".")
+        category = category_list[0]
+
+        if category == "cat":
+            category = 0
+        else:
+            category = 1
+
+        sample = {'image': image, 'category': category}
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+# transforms
+
+class Normalize(object):
+    """Convert a color image to grayscale and normalize color range to [0,1]."""
+
+    def __call__(self, sample):
+        image, category = sample['image'], sample['category']
+        image_copy = np.copy(image)
+
+        # convert image to grayscale
+        image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+        # scale color range from [0, 255] to [0, 1]
+        image_copy = image_copy/255
+
+        return {'image': image_copy, 'category': category}
+
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size"""
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        image, category = sample['image'], sample['category']
+
+        h, w = image.shape[:2]
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+        img = cv2.resize(image, (new_w, new_h))
+
+        return {'image': img, 'category': category}
+
+
+class ToTensor(object):
+    """Convert ndarrays to Tensors"""
+    def __call__(self, sample):
+        image, category = sample['image'], sample['category']
+
+        # if image has no grayscale color channel, add one
+        if len(image.shape) == 2:
+            # add that third color dim
+            image = image.reshape(image.shape[0], image.shape[1], 1)
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        image = image.transpose((2, 0, 1))
+
+        return {'image': torch.from_numpy(image), 'category': category}
